120725f46b458332393a919d0a05981b3d14ce1b
Stonnystone <okuta.omeiza@gmail.com>
2022-10-10 01:20:20 +0100

    1 file modified, 5 added

--- quasar.config.js
+++ quasar.config.js
@@ -27,7 +27,10 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
-    boot: [],
+    boot: [
+      'i18n',
+      'axios',
+    ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ['app.scss'],
